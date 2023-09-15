ca2dd5b6047f3b1e5e69f83207fb6902d94c5c89
Estudiantes <Estudiantes@ULELAB331>
2023-09-15 17:10:08 -0500

    Agregar archivos de proyecto.

---
+++ For4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ingrese la cantidad de numeros a sumar: ");
+int limite = int.Parse(Console.ReadLine());
+
+int suma = 0;
+
+for (int i = 1; i <= limite; i++)
+{
+    Console.Write("Ingrese el numero "+i+": ");
+    int numero = int.Parse(Console.ReadLine());
+    suma = suma + numero;
+}
+Console.WriteLine("la suma es: "+suma);
+Console.ReadLine();
